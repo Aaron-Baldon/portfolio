89a6633c1001b6aa9d250335376606c4b881273d
Aaron-Baldon <defnotjayyy06@gmail.com>
2026-06-11 00:08:16 +0800

    Initialize portfolio project with Next.js and Tailwind

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "@/components/shared/theme-provider";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "Creative Developer Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.variable} antialiased`}>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
